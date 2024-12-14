f3246a1baf5c4fc9f48bf8abad67f042479ec7d3
RitankJaikar <ritank.jaikar@gmail.com>
2024-12-14 13:51:27 +0530

    Submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [click, setClick] = useState(0);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <div>Button clicked {click} times</div>
+        <button onClick={() => setClick(click+1)}>Click Me</button>
     </div>
   )
 }
